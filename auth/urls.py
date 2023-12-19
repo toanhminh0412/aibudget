325c1140548c1ad32eaa9e22863dc9c6bf7e6e50
Minh To <toanhminh0412@gmail.com>
2023-12-18 22:58:14 -0800

    Add a home page and a login page

--- auth/urls.py
+++ auth/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.HomeView.as_view(), name="home"),
+    path("login/", views.LoginView.as_view(), name="login"),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
-def index(request):
-    return render(request, "auth/base.html")
+# Home page
+class HomeView(TemplateView):
+    template_name = "auth/home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home"
+        return context
+    
+# Login page
+class LoginView(TemplateView):
+    template_name = "auth/login.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Login"
+        return context
